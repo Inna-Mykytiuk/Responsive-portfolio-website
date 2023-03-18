e9fc76a94e1735592d560324209167d4e9fea53d
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-18 18:11:34 +0200

    add section navBar with styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+};
