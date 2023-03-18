69ccf9b94c141b8618e175b833f3f5b50937264a
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-18 22:59:50 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
+  );
+};
