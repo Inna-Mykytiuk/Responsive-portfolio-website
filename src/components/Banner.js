5bec3c659cf888e0d3a5ad1b44d56634e4c79584
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-19 00:45:07 +0200

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -60,7 +60,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagLine">Welcome to my Portfolio</span>
             <h1>
-              {`Hi Hi Hi I'm TokIo `}
+              {`Hi Hi I'm TokIo `}
               <span className="wrap">{currentText}</span>
             </h1>
             <p>
