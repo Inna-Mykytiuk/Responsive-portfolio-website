4c5163e17d689086568428f3523f0fdb4f00fb33
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-18 19:49:53 +0200

    add hero section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 
 export const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 };
---
+++ src/components/Banner.js
@@ -0,0 +1,81 @@
+//імпорт бібліотек та елементів
+import { Container, Row, Col } from 'react-bootstrap';
+//імпорт іконки
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+//імпорт героя
+import headerImg from '../assets/img/header-img.svg';
+import { useState, useEffect } from 'react';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ['Web Developer', 'Freelancer'];
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => clearInterval(ticker);
+  }, [text]);
+
+  const tick = () => {
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  };
+
+  return (
+    //розмітка секції
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagLine">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm TokIo `}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <button onClick={() => console.log('connect')}>
+              Let’s Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
