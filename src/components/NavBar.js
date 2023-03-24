9e9c385dbd4b1c2ca5dda2fc6b1a99eecb408c22
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-03-24 23:46:51 +0200

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ export const NavBar = () => {
         <Container>
           <Navbar.Brand href="/">
             {/* <img src={logo} alt="Logo" /> */}
-            <p className="logo">TokIo</p>
+            <p className="logo">Tokio</p>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
